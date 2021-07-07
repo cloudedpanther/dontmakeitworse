6334f12ecf7dc5c395fb6bf4d8c7bba99ba98629
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-07-07 10:04:47 +0900

    form submission
    get rid of handle function
    use submit
    required input and max length

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
 const loginInput = document.querySelector(".login-form input"),
       loginBtn = document.querySelector(".login-form button");
-
-function handleLoginBtnClick() {
-    console.log("hello", loginInput.value);
-}
-
-loginBtn.addEventListener("click", handleLoginBtnClick);
