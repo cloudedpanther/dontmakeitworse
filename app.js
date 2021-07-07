4278ea1315c80a3cf9fbc9affc9f201dda1c3d76
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-07-07 12:02:05 +0900

    getting username
    getting username
    hiding login form
    showing the greetings(new info)

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-const loginInput = document.querySelector(".login-form input"),
-      loginBtn = document.querySelector(".login-form button");
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form input");
+// const loginBtn = document.querySelector(".login-form button");
+const greeting = document.querySelector(".greeting");
+
+const HIDDEN = "hidden";
+
+function handleLoginSubmit(event) {
+    event.preventDefault();
+    const userName = loginInput.value;
+
+    greeting.innerText = `Hello, ${userName}!`;   
+    loginForm.classList.add(HIDDEN);
+    greeting.classList.remove(HIDDEN);
+}
+
+loginForm.addEventListener("submit", handleLoginSubmit);
